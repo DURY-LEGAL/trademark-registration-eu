7b76d913d8f27dfce7db607c7b98854e9bf633cd
Slarti @ DURY LEGAL <slarti@dury.de>
2026-05-03 22:41:01 +0200

    Initial commit: index.js

---
+++ index.js
@@ -0,0 +1,3 @@
+// trademark-registration-eu - EU trademark registration toolkit - EUIPO filing automation. Powered by DURY LEGAL.
+// Coming soon. Visit https://www.dury.de for more information.
+module.exports = {};
